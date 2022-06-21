07f8f569bbef49471213923a60d90c4f1e980034
rhenium <rhenium750@gmail.com>
2022-06-21 19:59:00 +0900

    update: use discord thread message

--- src/job/newItemNotification/configLoader.ts
+++ src/job/newItemNotification/configLoader.ts
@@ -3,26 +3,17 @@ import moment from "moment";
 import { Client } from "@notionhq/client";
 
 const {
-  NEW_ITEM_NOTIFICATION_DISCORD_WEBHOOK,
-  JOB_REPORT_DISCORD_WEBHOOK,
   NOTION_TOKEN,
   NOTION_DATABASE_ID,
   CHECK_INTERVAL,
   REQUEST_INTERVAL,
+  DISCORD_TOKEN,
+  DISCORD_GUILD_ID,
+  NEW_ITEM_NOTIFICATION_DISCORD_CHANNEL_ID,
+  JOB_REPORT_DISCORD_CHANNEL_ID,
 } = process.env;
 
 export async function loadConfig() {
-  if (typeof NEW_ITEM_NOTIFICATION_DISCORD_WEBHOOK !== "string") {
-    throw new Error(
-      `NEW_ITEM_NOTIFICATION_DISCORD_WEBHOOK is not string.NEW_ITEM_NOTIFICATION_DISCORD_WEBHOOK=${NEW_ITEM_NOTIFICATION_DISCORD_WEBHOOK}`
-    );
-  }
-  if (typeof JOB_REPORT_DISCORD_WEBHOOK !== "string") {
-    throw new Error(
-      `JOB_REPORT_DISCORD_WEBHOOK is not string.JOB_REPORT_DISCORD_WEBHOOK=${JOB_REPORT_DISCORD_WEBHOOK}`
-    );
-  }
-
   if (typeof NOTION_TOKEN !== "string") {
     throw new Error(`NOTION_TOKEN is not string.NOTION_TOKEN=${NOTION_TOKEN}`);
   }
@@ -30,6 +21,31 @@ export async function loadConfig() {
   if (typeof NOTION_DATABASE_ID !== "string") {
     throw new Error(
       `NOTION_DATABASE_ID is not string.NOTION_DATABASE_ID=${NOTION_DATABASE_ID}`
+    );
+  }
+
+  if (typeof DISCORD_TOKEN !== "string") {
+    throw new Error(
+      `DISCORD_TOKEN is not string.DISCORD_TOKEN=${DISCORD_TOKEN}`
+    );
+  }
+
+  if (typeof DISCORD_GUILD_ID !== "string") {
+    throw new Error(
+      `DISCORD_GUILD_ID is no
+      t string.DISCORD_GUILD_ID=${DISCORD_GUILD_ID}`
+    );
+  }
+
+  if (typeof NEW_ITEM_NOTIFICATION_DISCORD_CHANNEL_ID !== "string") {
+    throw new Error(
+      `NEW_ITEM_NOTIFICATION_DISCORD_CHANNEL_ID is not string.NEW_ITEM_NOTIFICATION_DISCORD_CHANNEL_ID=${NEW_ITEM_NOTIFICATION_DISCORD_CHANNEL_ID}`
+    );
+  }
+
+  if (typeof JOB_REPORT_DISCORD_CHANNEL_ID !== "string") {
+    throw new Error(
+      `JOB_REPORT_DISCORD_CHANNEL_ID is not string.JOB_REPORT_DISCORD_CHANNEL_ID=${JOB_REPORT_DISCORD_CHANNEL_ID}`
     );
   }
 
@@ -42,40 +58,45 @@ export async function loadConfig() {
     .subtract(checkInterval, "days");
   const newItemEndTime = moment().startOf("day");
 
+  const firstLinks = (
+    await new Client({ auth: NOTION_TOKEN }).databases.query({
+      database_id: NOTION_DATABASE_ID,
+    })
+  )?.results
+    .map((page) => {
+      return {
+        name: _.get(page, ["properties", "Name", "title", 0, "plain_text"]),
+        ownerId: _.get(page, [
+          "properties",
+          "OwnerId",
+          "rich_text",
+          0,
+          "plain_text",
+        ]),
+        recordId: _.get(page, [
+          "properties",
+          "RecordId",
+          "rich_text",
+          0,
+          "plain_text",
+        ]),
+        active: _.get(page, ["properties", "Active", "checkbox"]),
+      };
+    })
+    .filter(({ ownerId, recordId, active }) => ownerId && recordId && active);
+
   return {
-    newItemNotificationDiscordWebhook: NEW_ITEM_NOTIFICATION_DISCORD_WEBHOOK,
-    jobReportDiscordWebhook: JOB_REPORT_DISCORD_WEBHOOK,
     notionToken: NOTION_TOKEN,
     notionDatabaseId: NOTION_DATABASE_ID,
     checkInterval,
     requestInterval,
     newItemStartTime,
     newItemEndTime,
-    firstLinks: (
-      await new Client({ auth: NOTION_TOKEN }).databases.query({
-        database_id: NOTION_DATABASE_ID,
-      })
-    )?.results
-      .map((page) => {
-        return {
-          name: _.get(page, ["properties", "Name", "title", 0, "plain_text"]),
-          ownerId: _.get(page, [
-            "properties",
-            "OwnerId",
-            "rich_text",
-            0,
-            "plain_text",
-          ]),
-          recordId: _.get(page, [
-            "properties",
-            "RecordId",
-            "rich_text",
-            0,
-            "plain_text",
-          ]),
-          active: _.get(page, ["properties", "Active", "checkbox"]),
-        };
-      })
-      .filter(({ ownerId, recordId, active }) => ownerId && recordId && active),
+    firstLinks,
+    discordToken: DISCORD_TOKEN,
+    discordGuildId: DISCORD_GUILD_ID,
+    jobReportDiscordChannelId: JOB_REPORT_DISCORD_CHANNEL_ID,
+    newItemNotificationDiscordChannelId:
+      NEW_ITEM_NOTIFICATION_DISCORD_CHANNEL_ID,
   };
 }
--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,23 +1,86 @@
-import _ from "lodash";
-import axios from "axios";
-import { logWarn, logError } from "./log";
+import _, { identity } from "lodash";
 import { sleep } from "./util";
+import Discord, {
+  TextChannel,
+  AnyChannel,
+  MessagePayload,
+  MessageOptions,
+  Intents,
+  Message,
+} from "discord.js";
 
-export async function sendDiscordMessage(webhookUrl: string, body: any) {
-  while (true) {
-    try {
-      await axios.post(webhookUrl, body);
-      return;
-    } catch (e) {
-      const status = _.get(e, ["response", "status"]);
-      const retryAfter = _.get(e, ["response", "data", "retry_after"]);
-      if (status === 429 && retryAfter > 0) {
-        logWarn(`discord rate limit. retryAfter=${retryAfter}`);
-        await sleep(retryAfter + 500);
-      } else {
-        logError("discord webhook error.", e);
-        return;
-      }
+export default class DiscordClient {
+  private client = new Discord.Client({
+    intents: [Intents.FLAGS.GUILD_MESSAGES],
+  });
+  private isReady = false;
+
+  constructor(init: { token?: string }) {
+    this.client.once("ready", () => {
+      this.isReady = true;
+    });
+    this.client.login(init.token);
+  }
+
+  public static isTextChannel(
+    channel: undefined | null | AnyChannel
+  ): channel is TextChannel {
+    return _.has(channel, "send") !== undefined;
+  }
+
+  private async wait2Ready() {
+    while (!this.isReady) {
+      await sleep(0.1);
     }
+  }
+
+  public async getDiscordChannel(
+    guildId: string,
+    channelId: string
+  ): Promise<AnyChannel | null> {
+    await this.wait2Ready();
+
+    const guild = await this.client.guilds.fetch(guildId);
+    const channel = await guild.channels.fetch(channelId);
+    return channel;
+  }
+
+  public async sendDiscordMessage(
+    channel: AnyChannel,
+    message: string | MessagePayload | MessageOptions
+  ) {
+    await this.wait2Ready();
+
+    if (!DiscordClient.isTextChannel(channel)) {
+      throw new Error(`channel is not TextChannel.`);
+    }
+    channel.threads;
+
+    return channel.send(message);
+  }
+
+  public async sendDiscordThreadMessage(
+    targetMessage: Message,
+    message: string | MessagePayload | MessageOptions
+  ) {
+    await this.wait2Ready();
+
+    if (!targetMessage.hasThread) {
+      try {
+        const threadNameValid =
+          !!targetMessage.content && targetMessage.content != "";
+        await targetMessage.startThread({
+          name: threadNameValid ? targetMessage.content : "thread",
+        });
+      } catch (e) {}
+    }
+
+    if (targetMessage.hasThread) {
+      return await targetMessage.thread?.send(message);
+    }
+  }
+
+  public async destroy() {
+    await this.client.destroy();
   }
 }
